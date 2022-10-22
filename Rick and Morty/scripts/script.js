522a8ec7159481e8b23a446ecc4561e646c4957d
manpreet <manhspman5@gmail.com>
2022-10-22 10:38:14 +0200

    pushing some part of rick and morty exercise

--- Rick and Morty/scripts/script.js
+++ Rick and Morty/scripts/script.js
@@ -1,32 +1,56 @@
 //https://rickandmortyapi.com/api/episode
 
-/***** Fetching data from the api *****/
-const url = "https://rickandmortyapi.com/api/episode";
-fetch(url)
-  .then((episode) => episode.json())
-  .then((episodeObject) => {
-    console.log(episodeObject.results);
-    chaptersInfo(episodeObject);
-    charactersInfo(episodeObject);
-  });
+/***************  1.2 Web structure  ***************/
 
-/***** 1.1.1 : In this section you will obtain information about the chapters of Rick and Morty: *****/
+/* 1.21 Header : It contains Project Title */
+
+/** HEADER **/
+function headerDisplay() {
+  let div = document.createElement("div");
+  let h2 = document.createElement("H2");
+  let txt = document.createTextNode("Rick and Morty API");
+
+  div.classList.add("listItem");
+  h2.classList.add("mm");
+  div.appendChild(h2);
+
+  h2.appendChild(txt);
+  document.body.appendChild(h2);
+}
+headerDisplay();
+
+/** Episodes **/
+
+function g() {
+  const urlEpisodes = "https://rickandmortyapi.com/api/episode";
+  fetch(urlEpisodes)
+    .then((episode) => episode.json())
+    .then((episodeObject) => {
+      //  console.log(episodeObject.results);
+      chaptersInfo(episodeObject);
+      ShowEpisodeInfo(episodeObject);
+    });
+}
+
 function chaptersInfo(episodeObject) {
   episodeObject.results.forEach((episode) => {
     //console.log(episode.name);
     const root = document.querySelector("#root");
     const ul = document.createElement("ul");
     const li = document.createElement("li");
-    ul.appendChild(li);
-    li.innerHTML =
-      episode.name + "  " + episode.air_date + " " + episode.episode;
+    root.classList.add("root-list");
+    ul.classList.add("ul-list");
     root.appendChild(ul);
+    ul.appendChild(li);
+    li.innerHTML = "<a onclick='ShowEpisodeInfo()'>Episodes</a>";
+    //  episode.name + "  " + episode.air_date + " " + episode.episode;
   });
 }
 
-/***** 1.1.2: In this section you will obtain information about the characters of Rick and Morty: *****/
-function charactersInfo(episodeObject) {
-  episodeObject.results.forEach((c) => {
-    console.log(c.characters);
+function ShowEpisodeInfo(episodeObject) {
+  console.log("hiii");
+  episodeObject.results.forEach((episode) => {
+    console.log(episode.name);
   });
 }
+g();
